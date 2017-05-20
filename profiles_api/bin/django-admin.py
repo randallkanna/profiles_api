b5fb2809dbdb5f07140c56acae958fbfaa2145fc
randallkanna <randallkanna@gmail.comm>
2017-05-20 16:23:05 -0700

    initial commit

---
+++ profiles_api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/randallkanna/Documents/code/profiles_api/profiles_api/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
